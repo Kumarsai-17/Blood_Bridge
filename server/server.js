7c9507d7002a2f69eab1b78757b74bac9f43d48e
Kumarsai-17 <paidikumarsai@gmail.com>
2026-02-18 01:51:46 +0530

    Issues solved-2

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173", 
   "http://localhost:5174",
-  "https://blood-bridge-mu.vercel.app",
+  "https://blood-bridge-mu.vercel.app/api",
   process.env.FRONTEND_URL
 ].filter(Boolean); // Remove undefined values
 

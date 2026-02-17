43c8ade71f8fd12a98efafc0557f66b3f2a3ffd2
Kumarsai-17 <paidikumarsai@gmail.com>
2026-02-17 22:07:59 +0530

    Switch to Nodemailer, cleanup unused files, add email tests-3

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,13 +3,16 @@ const nodemailer = require("nodemailer");
 // Create Gmail transporter
 const createGmailTransporter = () => {
   return nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
 };
+
 
 
 // Send email utility
